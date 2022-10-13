748b12d711963c63adc167e6043424b3c47f7e1c
luciofreitas <luciodfp@gmail.com>
2022-10-13 00:29:12 -0300

    projectcontroller without jpa

--- src/main/java/controller/projectController.java
+++ src/main/java/controller/projectController.java
@@ -14,43 +14,43 @@ import util.connectionFactory;
 
 public class projectController {
 
-    private final EntityManager entityManager = null;
-    public void save(Project project) {
-
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistenceUnit");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-
-        entityManager.getTransaction().begin();
-        entityManager.persist(project);
-        entityManager.getTransaction().commit();
-
-        entityManager.close();
-        entityManagerFactory.close();
-    }
+//    private final EntityManager entityManager = null;
 //    public void save(Project project) {
-//        String sql = "INSERT INTO project (name, "
-//                + " description, "
-//                + " createdAt, "
-//                + " updatedAt) VALUES (?, ?, ?, ?)";
 //
-//        Connection connection = null;
-//        PreparedStatement statement = null;
+//        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistenceUnit");
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
 //
-//        try {
-//            connection = connectionFactory.getConnection();
-//            statement = connection.prepareStatement(sql);
+//        entityManager.getTransaction().begin();
+//        entityManager.persist(project);
+//        entityManager.getTransaction().commit();
 //
-//            statement.setString(1, project.getName());
-//            statement.setString(2, project.getDescription());
-//            statement.setDate(3, new Date(project.getCreatedAt().getTime()));
-//            statement.setDate(4, new Date(project.getUpdatedAt().getTime()));
-//            statement.execute();
-//        } catch (Exception ex) {
-//            throw new RuntimeException("Erro ao inserir o projeto " + ex.getMessage(), ex);
-//        } finally {
-//            connectionFactory.closeConnection(connection, statement);
-//        }
+//        entityManager.close();
+//        entityManagerFactory.close();
 //    }
+    public void save(Project project) {
+        String sql = "INSERT INTO project (name, "
+                + " description, "
+                + " createdAt, "
+                + " updatedAt) VALUES (?, ?, ?, ?)";
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+            connection = connectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+
+            statement.setString(1, project.getName());
+            statement.setString(2, project.getDescription());
+            statement.setDate(3, new Date(project.getCreatedAt().getTime()));
+            statement.setDate(4, new Date(project.getUpdatedAt().getTime()));
+            statement.execute();
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao inserir o projeto " + ex.getMessage(), ex);
+        } finally {
+            connectionFactory.closeConnection(connection, statement);
+        }
+    }
 
     public void update(Project project) {
         String sql = "UPDATE project SET"
